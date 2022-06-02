68ad84141e34456bb3c4f3473d930b24cc0d4870
Anthony Garcia <anthonydeyvis32@gmail.com>
2022-06-02 14:55:45 -0400

    change counter store in counter view

--- src/views/CounterView.vue
+++ src/views/CounterView.vue
@@ -1,3 +1,14 @@
 <template>
   <h1>Counter view</h1>
+  <button @click="counterStore.increment(1)">+1</button>
+  <button @click="counterStore.increment(2)">+2</button>
+  <button @click="counterStore.increment(5)">+5</button>
 </template>
+
+<script lang="ts" setup>
+
+import { useCounterStore } from '../stores/counter'
+
+const counterStore = useCounterStore()
+
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,16 @@
 <template>
   <h1>Home View</h1>
+  <p>
+    <b>Counter: </b> {{ counterStore.counter }}
+  </p>
+  <p>
+    <b>Number of changes: </b> {{ counterStore.numberOfChanges }}
+  </p>
 </template>
+
+<script lang="ts" setup>
+import { useCounterStore } from '../stores/counter'
+
+const counterStore = useCounterStore()
+
+</script>
